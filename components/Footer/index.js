705262b04d135dd02a4cc169d745d9a0b36ef0b3
Jainish <69842037+jainishrpandya@users.noreply.github.com>
2024-06-25 12:06:21 +0530

    Update index.js

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -24,8 +24,7 @@ const Footer = ({}) => {
         </div>
       </div>
       <h1 className="text-sm text-bold mt-2 laptop:mt-10 p-2 laptop:p-0">
-        Made With ❤ by{" "}
-          <a className="underline underline-offset-1">Jainish Pandya</a>
+        Made With ❤ by Jainish Pandya
       </h1>
     </>
   );
